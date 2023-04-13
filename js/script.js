e3d8705033290fcae5ae02286c7a2aee613db284
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-04-13 14:36:58 +0000

    DonE!

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 /**
  * This function calculates Volume of Triangula Pyramid.
  */
-function calculate () {
+function calculate() {
   // input
   let height = parseFloat(document.getElementById('height-of-pyramid').value);
   let baseEdge = parseFloat(document.getElementById('base-edge-of-pyramid').value);
@@ -17,6 +17,5 @@ function calculate () {
   let baseArea= baseEdge**2
   // output
   document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm²'
-}
-document.getElementById('baseArea').innerHTML = 'Base Area is: ' + baseArea.toFixed(2) + ' cm²'
+    document.getElementById('baseArea').innerHTML = 'Base Area is: ' + baseArea.toFixed(2) + ' cm²'
 }
